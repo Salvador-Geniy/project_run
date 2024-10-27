3aab908e1c880c56ebb90002b200d57e6ea6d58c
Alexey Derendyaev <alexderendyaev@yandex.ru>
2024-10-27 18:33:30 -0300

    ref

--- app_run/models.py
+++ app_run/models.py
@@ -1,1 +1,14 @@
 from django.db import models
+
+
+class ClubData(models.Model):
+    company_name = models.CharField(max_length=255, unique=True, verbose_name="название")
+    slogan = models.CharField(max_length=100, verbose_name="слоган")
+    contacts = models.CharField(max_length=255, verbose_name="адрес")
+
+    def __str__(self):
+        return self.company_name
+
+    class Meta:
+        verbose_name = "клуб"
+        verbose_name_plural = "клубы"
