25b6cef3583544804d087251587d9ff1d0f2f6ec
Alexey Derendyaev <alexderendyaev@yandex.ru>
2024-10-27 18:10:20 -0300

    CompanyData added

--- app_run/views.py
+++ app_run/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
-# Create your views here.
+
+@api_view(["GET"])
+def get_club_data(request):
+    return Response({
+        "company_name": "My company",
+        "slogan": "My slogan",
+        "contacts": "My company contacts",
+    }, 200)
+
