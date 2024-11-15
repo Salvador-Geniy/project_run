bf312d1789ce119f47604c0803ffff478b17a835
Alexey Derendyaev <alexderendyaev@yandex.ru>
2024-11-15 19:40:55 -0300

    Test

--- app_run/views.py
+++ app_run/views.py
@@ -132,7 +132,7 @@ class RunStopView(APIView):
         return Position.objects.filter(run=run)
 
     def update_run_fields(self, run: Run, dist_total: float, run_time_seconds: int, avg_speed: float) -> None:
-        run.distance = dist_total
+        # run.distance = dist_total
         run.status = "finished"
         run.run_time_seconds = run_time_seconds
         run.speed = avg_speed
