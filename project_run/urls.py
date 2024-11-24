8d1ccff18b2b5d9dee10186a0a46db10b9766150
Alexey Derendyaev <alexderendyaev@yandex.ru>
2024-11-24 19:33:11 -0300

    Analytics

--- project_run/urls.py
+++ project_run/urls.py
@@ -50,6 +50,6 @@ urlpatterns = [
     path("api/challenges/", ChallengeListView.as_view(), name="challenge-list"),
     path("api/challenges_summary/", ChallengesSummary2.as_view(), name="challenges-summary"),
     path("api/analytics_for_coach/<int:coach_id>/", CoachAnalytics.as_view(), name="coach-analitics"),
-    path("api/test-position", TestPositionView.as_view(), name="test-position"),
+    path("api/test-position/", TestPositionView.as_view(), name="test-position"),
     path("", include(router.urls)),
 ]
