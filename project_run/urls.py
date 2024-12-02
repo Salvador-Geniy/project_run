82c8e7d25ff6c0330a5e0f3969014affbedcc1a4
Alexey Derendyaev <alexderendyaev@yandex.ru>
2024-12-02 16:28:57 -0300

    UploadView

--- project_run/urls.py
+++ project_run/urls.py
@@ -52,6 +52,6 @@ urlpatterns = [
     path("api/challenges_summary/", ChallengesSummary2.as_view(), name="challenges-summary"),
     path("api/analytics_for_coach/<int:coach_id>/", CoachAnalytics.as_view(), name="coach-analytics"),
     path("api/upload_file/", UploadFileView.as_view(), name="upload-file"),
-    # path("api/unit-location/", UnitLocationListView.as_view(), name="unit-location"),
+    path("api/unit-location/", UnitLocationListView.as_view(), name="unit-location"),
     path("", include(router.urls)),
 ]
