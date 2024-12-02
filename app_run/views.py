3485663515b33a88a0ff1baa64e98dcb90a15c8f
Alexey Derendyaev <alexderendyaev@yandex.ru>
2024-12-02 14:39:07 -0300

    UploadView

--- app_run/views.py
+++ app_run/views.py
@@ -271,7 +271,7 @@ class UploadFileView(APIView):
                 return Response("Wrong content type", 400)
 
             wrong_rows = self.get_rows(file)
-        return JsonResponse(wrong_rows, safe=False, status=status.HTTP_200_OK)
+        return JsonResponse(data=wrong_rows, safe=False, status=status.HTTP_200_OK)
         # return Response("wer", 200)
 
     def get_rows(self, file):
