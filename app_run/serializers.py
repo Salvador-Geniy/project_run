453869518d8cbe6e16a887b729293aa951913f43
Alexey Derendyaev <alexderendyaev@yandex.ru>
2025-02-13 12:57:26 -0300

    ref

--- app_run/serializers.py
+++ app_run/serializers.py
@@ -66,6 +66,7 @@ class UserSerializer(ModelSerializer):
     runs_finished = IntegerField(read_only=True)
     items = SerializerMethodField()
     rating = SerializerMethodField()
+    date_joined = DateTimeField(format="%Y-%m-%dT%H:%M:%S.%f", read_only=True)
 
     class Meta:
         model = User
